a05184d49bb5cbbbba1ab569ee9e51165f0c34db
ayeshanasiba <ayeshanasiba11@gmail.com>
2023-08-26 22:59:55 +0600

    payment and shirt measurement

--- TailorApp/models.py
+++ TailorApp/models.py
@@ -10,3 +10,16 @@ class user_model(models.Model):
     u_cont_number = models.CharField(max_length=11)
     u_gender = models.CharField(max_length=10)
     password = models.CharField(max_length=30)
+
+class payment_model(models.Model):
+    p_id = models.IntegerField(primary_key=True)
+    p_method = models.CharField(max_length=20)
+    
+
+class measurementForShirt_model(models.Model):
+    s_productID = models.IntegerField(primary_key=True)
+    s_neck = models.IntegerField()
+    s_sleeve = models.IntegerField()
+    s_chest = models.IntegerField()
+    s_waist = models.IntegerField()
+    s_length = models.IntegerField()
