a401430afad58c7da589b775db619945237b9f6a
Dipto755 <akteruzzamandipto257@gmail.com>
2023-08-26 16:12:06 +0600

    adding user

--- TailorApp/models.py
+++ TailorApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class user_model(models.Model):
+    u_id = models.IntegerField(primary_key=True)
+    u_name = models.CharField(max_length=30)
+    u_email = models.EmailField(max_length=30)
+    u_address = models.TextField()
+    u_cont_number = models.CharField(max_length=11)
+    u_gender = models.CharField(max_length=10)
+    password = models.CharField(max_length=30)
