be21f3a688ea5783ae520a6c28ebfe318859b9ef
Shajid-Chowdhury-1024 <shajidchowdhury903@gmail.com>
2023-09-17 01:31:58 +0600

    registration

--- TailorApp/views.py
+++ TailorApp/views.py
@@ -14,5 +14,22 @@ def login_view(request):
 
 def signup_view(request):
     
+    if request.method == 'POST':
+        username = request.POST['username']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        
+        myuser = User.objects.create_user(username, email, password1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        
+        myuser.save()
+        
+        messages.success(request, "Your account has been successfully created.")
+        
+        return redirect('login')
     
     return render(request, 'signup.html')
